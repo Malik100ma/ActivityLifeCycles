b94ea6327a93e45f3ab4d888ec21bb29d4a97d77
Student_1 <codecat.ua@gmail.com>
2026-08-15 17:14:00 +0300

    sas

--- app/src/main/java/com/malik100ma/activitylifecycles/MainActivity.java
+++ app/src/main/java/com/malik100ma/activitylifecycles/MainActivity.java
@@ -1,10 +1,14 @@
 package com.malik100ma.activitylifecycles;
 
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.util.Log;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -14,18 +18,18 @@ public class MainActivity extends AppCompatActivity {
 
     protected String TAG = "LIFE_CYCLE_DEBUG";
 
-    protected TextView tvResumeCount, tvCreateCount, tvStartCount, tvPauseCount, tvStopCount;
+    protected TextView totalCreateCount, tvCreateCount;
 
     int onCreateCount = 0;
 
-    int onResumeCount = 0;
+    int onTotalCreateCount = 0;
 
-    int onStartCount = 0;
 
-    int onPauseCount = 0;
+    SharedPreferences sharedPreferences;
+    private static final String KEY_PREFERENCES = "total_creates";
 
-    int onStopCount = 0;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,52 +40,57 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        tvCreateCount = findViewById(R.id.tvCreateCount);
+
 
         Log.d(TAG, "OnCreate");
-        onCreateCount++;
-        tvCreateCount.setText(onCreateCount);
+        sharedPreferences = getSharedPreferences("livecycles_prefs", MODE_PRIVATE);
+        if (savedInstanceState != null) {
+            onCreateCount = savedInstanceState.getInt("keyOnCreate");
+        }
+        onTotalCreateCount = sharedPreferences.getInt(KEY_PREFERENCES,0);
+        onCreateCount += 1;
+        tvCreateCount = findViewById(R.id.tvCreateCount);
+        totalCreateCount = findViewById(R.id.totalCreateCount);
+        tvCreateCount.setText(String.valueOf(onCreateCount));
+        totalCreateCount.setText(String.valueOf(onTotalCreateCount));
+
+        sharedPreferences.edit().putInt(KEY_PREFERENCES, onTotalCreateCount).apply();
     }
+
 
     @Override
     protected void onStart() {
         super.onStart();
         Log.d(TAG, "onStart");
-        tvStartCount = findViewById(R.id.tvOnStartCount);
-        onStartCount++;
-        tvStartCount.setText(onStartCount);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         Log.d(TAG, "onResume");
-        tvResumeCount = findViewById(R.id.tvResumeCount);
-        onResumeCount++;
-        tvResumeCount.setText(onResumeCount);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        tvPauseCount = findViewById(R.id.tvPauseCount);
         Log.d(TAG, "onPause");
-        onPauseCount++;
-        tvPauseCount.setText(onPauseCount);
     }
 
     @Override
     protected void onStop() {
         super.onStop();
         Log.d(TAG, "onStop");
-        tvResumeCount = findViewById(R.id.tvStopCount);
-        onStopCount++;
-        tvStopCount.setText(onStopCount);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         Log.d(TAG, "OnDestroy");
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("keyOnCreate", onCreateCount);
     }
 }
